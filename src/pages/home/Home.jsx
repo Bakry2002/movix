88834b71dbca0e468b06d5f0574ad0be87437b17
Abdullah M.Bakry <abdaullah62@gmail.com>
2023-05-04 02:39:49 +0300

    final update on home page v1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,16 @@
 import './style.scss'
 import { HeroBanner } from './heroBanner/HeroBanner'
 import { Trending } from './trending/Trending'
+import { Popular } from './popular/Popular'
+import { TopRated } from './topRated/TopRated'
 
 export const Home = () => {
   return (
     <div className="home-page">
         <HeroBanner />
         <Trending />
-        <div style={{height: 500}}></div>
+        <Popular />
+        <TopRated />
     </div>
   )
 }
